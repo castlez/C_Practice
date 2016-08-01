bfd21313449b4d3a95cb0e32c35731d5f022f7d7
castlez <castlez93@gmail.com>
2016-08-01 04:04:08 -0700

    broke it, errors about type declarations

--- Char_Prof/main.c
+++ Char_Prof/main.c
@@ -7,7 +7,9 @@ struct character {
     char ** inven;
 };
 
-
+//prototypes silly
+void create_character();
+void sv_character(char[], char*[]);
 void main(){
     
     printf("Greetings, welcome to Character Profile...r\n"\
@@ -22,7 +24,7 @@ void main(){
 
     switch(ans) {
         case '1' :
-            printf("TODO 1\n");
+            create_character();
             break;
         case '2' :
             printf("TODO 2\n");
@@ -30,10 +32,36 @@ void main(){
         case '3' : 
             printf("bye\n");
             return;
-    }
-     
-    
-
-
-    		
+    }   
 }
+
+void create_character(){
+    char ntemp[256]; 
+    printf("\nName >> ");
+    scanf("%s", ntemp);
+
+    char *itemp[10] = malloc(10*256*sizeof(char));
+    int i;
+    for(i = 0 ; i < 10;i++){//10 inven slots
+        printf("Item %d (%d left)",i+1,10-i);
+        scanf("%s",itemp[i]);
+    }
+
+    sv_character(ntemp, itemp);
+    prinf("\n|DONE|\n");
+
+}
+
+void sv_character(char na[], char *inv[]){
+    FILE * fil = fopen("char_list.txt", "w");
+    fprintf(fil, "\n%s\n", na);
+    int i;
+    for(i = 0;i<sizeof(inv);i++){
+        fprintf(fil, "%s\n", inv[i]);
+    }
+    fclose(fil);
+    printf("DONE\n");
+}
+
+
+
